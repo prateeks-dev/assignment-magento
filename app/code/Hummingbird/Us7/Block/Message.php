6a04e70625d5acba697adbef41ffa1e9249a5edd
Prateek Sahu <prateek.sahu@hbwsl.com>
2022-04-05 07:44:13 +0530

    User Story7

---
+++ app/code/Hummingbird/Us7/Block/Message.php
@@ -0,0 +1,19 @@
+<?php
+namespace Hummingbird\Us7\Block;
+class Message extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+    {
+        parent::__construct($context);
+    }
+
+    public function getMessage()
+    {
+        return __('This is a message');
+    }
+
+    public function _afterToHtml($html)
+    {
+        return parent::_afterToHtml($html . "<h6>This is from after to html method</h6>");
+    }
+}
---
+++ app/code/Hummingbird/Us72/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Hummingbird_Us72', __DIR__);
